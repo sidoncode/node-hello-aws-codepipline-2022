953d9c87608579a04f33b0ed4682d84dae798e58
Siddharth Sharma <siddharth54330@gmail.com>
2023-02-12 19:40:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello SkillSequel, From Sid, India! - This is the Manual Deployment\n'
+  const msg = 'Hello This is an Example of CICD - Pipeline\n'
   res.end(msg);
 });
 
