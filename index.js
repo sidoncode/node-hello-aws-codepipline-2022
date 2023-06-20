dabd33916a9b68fc044fdcd50c565d08c145adf1
Siddharth Sharma <siddharth54330@gmail.com>
2023-06-20 11:44:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Manual Deployment :)'
+  const msg = 'Automated Deployment - MRU'23 :)'
   res.end(msg);
 });
 
